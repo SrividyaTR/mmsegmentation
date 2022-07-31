90c3403d373b66929e58af9eeaa5009014299ed1
srividyatr <srividya.tr@gmail.com>
2022-07-31 11:10:51 +0200

    add noise immediately after cca

--- mmseg/models/decode_heads/cc_head.py
+++ mmseg/models/decode_heads/cc_head.py
@@ -37,10 +37,9 @@ class CCHead(FCNHead):
         output = self.convs[0](x)
         for _ in range(self.recurrence):
             output = self.cca(output)
+        output = output + (0.1 ** 0.5)*torch.randn(output.size()).cuda()
         output = self.convs[1](output)
         print('Output shape after cca and selfconv', output.shape)
-
-        output = output + (0.1 ** 0.5)*torch.randn(output.size()).cuda()
 
         if self.concat_input:
             output = self.conv_cat(torch.cat([x, output], dim=1))
